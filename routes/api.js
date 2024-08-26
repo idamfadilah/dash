5219137d15627763ab1d628a85f152e61867ac2e
idam fadilah <idamfadilah.9c@gmail.com>
2024-08-26 12:48:56 +0700

    chore: Add uuid npm dependency for generating unique IDs

--- routes/api.js
+++ routes/api.js
@@ -3,28 +3,42 @@ const fs = require('fs');
 var express = require('express');
 var router = express.Router();
 const jwt = require('jsonwebtoken');
+const uuid = require('uuid');
 
 const SECRET_KEY = 'It was secret, but now it is not.';
 
 const users = {
     "users": [
         {
-            "id": "1",
+            "id": "cc960e84-2dbe-4a27-8eb5-edf2e478ef18",
             "name": "John Doe",
             "email": ""
         },
         {
-            "id": "2",
+            "id": "1966daa0-38d3-46a6-9a47-65d805bf2ffd",
             "name": "Jane Doe",
             "email": ""
         }
     ]
 }
 
+function verifyToken(req, res, next) {
+    var token = req.cookies.secureCookie
+    if (!token) {
+        return res.status(401).send({ auth: false, message: 'No token provided.' });
+    }
+    jwt.verify(token.replaceAll('"', ''), SECRET_KEY, function (err, decoded) {
+        if (err) {
+            return res.status(500).send({ auth: false, message: err });
+        }
+        next();
+    });
+}
+
+
 
 router.post('/login', async (req, res) => {
     const { username, password } = req.body;
-    console.log(username, password);
     if (username === 'admin' && password === 'admin') {
         const dataToSecure = {
             dataToSecure: "This is the secret data in the cookie.",
@@ -36,84 +50,58 @@ router.post('/login', async (req, res) => {
             expires: dayjs().add(30, "days").toDate(),
         });
         res.status(200).json({ token: token });
+    } else {
+        res.status(401).json({ error: 'Unauthorized' });
     }
-    res.status(401).json({ error: 'Unauthorized' });
 })
 
 // Get all users
-router.get('/users', (req, res) => {
-    console.log(req.cookies.secureCookie);
-    try{
-        const cookie = jwt.verify(req.cookies.secureCookie.replaceAll('"',''), SECRET_KEY);
-        console.log(cookie);
-        if (cookie) {
-            res.json(users);
-        }
-    }catch(err){
-        console.log(err);
-    }
-    res.status(401).json({ error: 'Unauthorized' });
+router.get('/users', verifyToken, (req, res) => {
+    res.json(users);
 });
 
 // Get a specific user by ID
-router.get('/users/:id', (req, res) => {
+router.get('/users/:id', verifyToken, (req, res) => {
     const userId = req.params.id;
-    const cookie = jwt.verify(req.cookies.secureCookie.replaceAll('"',''), SECRET_KEY);
-    if (cookie) {
-        const user = users.users.find(user => user.id === userId);
-        if (user) {
-            res.json(user);
-        } else {
-            res.status(404).json({ error: 'User not found' });
-        }
+    const user = users.users.find(user => user.id === userId);
+    if (user) {
+        res.json(user);
+    } else {
+        res.status(404).json({ error: 'User not found' });
     }
-
-    res.status(401).json({ error: 'Unauthorized' });
 });
 
 // Create a new user
-router.post('/users', (req, res) => {
+router.post('/users', verifyToken, (req, res) => {
     const newUser = req.body;
-    const cookie = jwt.verify(req.cookies.secureCookie.replaceAll('"',''), SECRET_KEY);
-    if (cookie) {
-        users.users.push(newUser);
-        res.json(newUser);
-    }
-    res.status(401).json({ error: 'Unauthorized' });
+    newUser.id = uuid.v4();
+    users.users.push(newUser);
+    res.json(newUser);
 });
 
 // Update an existing user
-router.put('/users/:id', (req, res) => {
-    const cookie = jwt.verify(req.cookies.secureCookie.replaceAll('"',''), SECRET_KEY);
-    if (cookie) {
-        const userId = req.params.id;
-        const index = users.users.findIndex(user => user.id === userId);
-        if (index !== -1) {
-            users.users[index] = req.body;
-            res.json(users.users[index]);
-        }
+router.put('/users/:id', verifyToken, (req, res) => {
+    const userId = req.params.id;
+    const index = users.users.findIndex(user => user.id === userId);
+    if (index !== -1) {
+        users.users[index] = req.body;
+        res.json(users.users[index]);
     }
-    res.status(401).json({ error: 'Unauthorized' });
 });
 
 // Delete a user
-router.delete('/users/:id', (req, res) => {
-
-    const cookie = jwt.verify(req.cookies.secureCookie.replaceAll('"',''), SECRET_KEY);
-    if (cookie) {
-        const userId = req.params.id;
-        const index = users.users.findIndex(user => user.id === userId);
-        if (index !== -1) {
-            users.users.splice(index, 1);
-            res.json({ message: 'User deleted' });
-        }
+router.delete('/users/:id', verifyToken, (req, res) => {
+    const userId = req.params.id;
+    const index = users.users.findIndex(user => user.id === userId);
+    if (index !== -1) {
+        users.users.splice(index, 1);
+        res.json({ message: 'User deleted' });
     }
-    res.status(401).json({ error: 'Unauthorized' });
 });
 
 
 // Logout
-router.post('/logout', (req, res) => {
+router.post('/logout', verifyToken, (req, res) => {
     res.clearCookie('secureCookie');
     res.status(200).json({ message: 'Logged out successfully' });
 });
